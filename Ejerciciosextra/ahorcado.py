68b923dbfe0af676a5b5d034d84e1f9e6132b04f
Roberto Cantero <robertocanterogomez@gmail.com>
2026-04-16 20:04:26 +0200

    Trabajo realizado el día 16 de abril

--- Ejerciciosextra/ahorcado.py
+++ Ejerciciosextra/ahorcado.py
@@ -1,34 +1,59 @@
 # Quiero crear un juego del ahorcado
 # ¿Qué es lo que hay que hacer? ¿Cuáles son los pasos?
-# Pedir al usuario una letra
+# Crear una lista de palabras y elegir una aleatoria
 # Para hacerlo más sencillo, que la palabra no tenga más de 5 letras
-# Habrá 3 vidas, es decir, el bucle irá quitando vidas si el caracter no está
-# en la palabra a adivinar
+# Habrá 3 vidas, es decir, el bucle tendrá la condición de que mientras las vidas sean > 3, 
+# seguirá funcionando
 # Claro, hace falta un input del usuario pero también una palabra a 
 # adivinar. ¿Cómo se hace esto último?
 
-# Pedir al usuario una letra
-palabra = input("¡Bienvenido al ahorcado! Adivina la palabra de 5 letras que estoy pensando. Dime una letra: ")
+# A mitad me he dado cuenta de que hay que utilizar esta librería
+import random
 
-# Crear la función para que salga una palabra aleatorio a adivinar
-def palabra_aleatoria():
-    palabra = ""
-    return palabra
+# Crear una lista con las palabras a adivinar y elegir una al azar
 
-# 
+conjunto_palabras = ["amigo", "coche", "juego", "cosas", "usado", "fuego", "grupo"]
+palabra = random.choice(conjunto_palabras)
 
-# Comprobar con un bucle si la letra introducida está en la palabra
-# Si se falla, una vida menos
-# Si se acierta, las vidas se mantienen igual y se imprime la posicion de la letra junto con _.
-# Por ejemplo, si la palabra es camas y se acierta la letra m, se devolvería _ _ m _ _.
-# También hay que imprimir, se acierte o se falle, el número de vidas restantes.
-vidas = 3
-for letra in palabra:
-    if letra in palabra:
-        vidas = vidas
-        print(f"Has acertado. Te quedan {vidas} vidas.")
+# Creo las variables fundamentales en el juego (nº de vidas y las letras que se han adivinado ya)
+
+vidas = 3 # empiezo con 3 vidas
+letras_adivinadas = [] # voy guardando en una lista las letras que he acertado
+
+# Creo el bucle del juego (el lío gordo)
+
+
+while vidas > 0: # mientras las vidas sean mayores de cero
+    progreso = "" # lo primero que hago es crear una variable string vacía
+    for i in palabra: #ahora iteramos sobre la palabra a adivinar 
+        if i in letras_adivinadas: # si la letra aparece en la palabra
+            progreso += i + " " # la añado a progreso
+        else:
+            progreso += "_ " # de lo contrario pongo _
+
+    print(f"\nPalabra: {progreso}") # imprimo lo que tengo hasta ahora
+    print(f"Vidas restantes: {vidas}") # imprimo las vidas que me quedan
+
+    if "_" not in progreso: # si en la palabra progreso no hay _
+        print("¡Felicidades! Has ganado.") # imprimo la felicitación
+        break # paro el programa, porque el usuario ha ganado   
+
+    intento = input("Dime una letra: ").lower() # pido otra letra
+
+    if intento in palabra: # itero con la nueva letra
+        if intento not in letras_adivinadas: # si la nueva letra no la he dicho
+            letras_adivinadas.append(intento) # añadela a los intentos
+            print(f"¡Bien hecho! La letra '{intento}' está en la palabra.") # imprime 
+        else:
+            print("Ya habías dicho esa letra.") # dime que ya la había dicho
     else:
-        vidas = vidas - 1
-        print(f"Has fallado. Te quedan {vidas} vidas.")
+        vidas -= 1 # quita una vida
+        print(f"Lo siento, la '{intento}' no está.") # imprime el mensaje de fallo
+
+# ¿Qué pasa si el while deja de funcionar? Que el usuario ha perdido, con lo que solo queda
+# comprobar que las vidas son cero e imprimir el mensaje de game over.
+
+if vidas == 0: # cuando las vidas llegan a cero
+    print(f"\n¡Game Over! Te has quedado sin vidas. La palabra era: {palabra}") # mensaje de fin de juego
 
 
